8c56edbfbe801a461155923219b1c10db97ec3d6
Vance Miller <vbmiller@cs.unc.edu>
2016-04-24 13:24:30 +0000

    remove unused headers

--- Samples/mem.h
+++
@@ -1,6 +0,0 @@
-void memtest_init(int sync_level, int numElements);
-void memtest_alloc(int numElements);
-void memtest_copyin(int numElements);
-void memtest_copyout(int numElements);
-void memtest_cudafree();
-void memtest_cleanup();
--- Samples/mm.h
+++
@@ -1,9 +0,0 @@
-void mm_init(int sync_level);
-void mm_mallocHost(int numElements);
-void mm_cudaMalloc(int numElements);
-void mm_copyin(int numElements);
-void mm_exec(int numElements);
-void mm_copyout();
-void mm_cudaFree();
-void mm_freeHost();
-void mm_finish();
