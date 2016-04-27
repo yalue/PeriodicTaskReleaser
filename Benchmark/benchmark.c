571158fc50d06b1c334759a184e21c3051a3da44
Vance Miller <vbmiller@cs.unc.edu>
2016-04-27 20:22:16 +0000

    Add sync option.
    Added command line option to specify CUDA synchronization with CPU. Cleaned up help interface.

--- Benchmark/benchmark.c
+++ Benchmark/benchmark.c
@@ -13,21 +13,29 @@
 #define DEFAULT_DATA_SIZE 1024 // 2^10
 #define DEFAULT_EXPERIMENT_DURATION 60000 // 1 minute 
 #define DEFAULT_ITERATION_COUNT 10000
+#define DEFAULT_SYNC 2
+
+#define FIVE_MS_IN_NS 5000000 // 5 million
 
 /*
  * Program Args
  */
 const char *argp_program_version = "v1";
-const char *argp_program_bug_address = "<vbmiller@cs.unc.edu>";
+const char *argp_program_bug_address = "<vbmiller+gpusync@cs.unc.edu>";
 static char doc[] = "GPU Sample Program Benchmarking.";
 static char args_doc[] = "";
 static struct argp_option options[] = {
+  {0, 0, 0, 0, "Experiment configuration parameters:"},
   {"duration", 'd', "experiment_duration", OPTION_HIDDEN, "Specifies the duration the experiment should run in milliseconds."},
   {"iterations", 'n', "iteration_count", 0, "Specifies the number of iterations to run before benchmarking is complete."},
   {"size", 's', "data_size", 0, "Specifies the size of input data to the task. Some tasks may disregard this value."},
+  {"sync", 'y', "{0|1}", 0, "Specifies how the CPU should synchronize with the GPU kernel. {0: spin, 1: yield, default: block}."},
+
+  {0, 0, 0, 0, "Mutual exclusive choice of GPU operations to benchmark:"},
   {"all", 'a', 0, OPTION_ARG_OPTIONAL, "Benchmarks the entire program, excluding setup and teardown. This is the default option."},
   {"copy", 'c', 0, OPTION_ARG_OPTIONAL, "Benchmarks the copy engine use only."},
   {"exec", 'e', 0, OPTION_ARG_OPTIONAL, "Benchmarks the execution engine use only."},
+
   {0},
 };
 
@@ -36,6 +44,7 @@ struct arguments {
   int experiment_duration;
   int iteration_count;
   int operation;
+  int sync;
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
@@ -68,6 +77,12 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
     case 'e':
       arguments->operation = EXEC_OPERATIONS;
       break;
+    case 'y':
+      arguments->sync= atoi(arg);
+      if (arguments->sync < 0 || arguments->sync > 2) {
+        return EINVAL;
+      }
+      break;
     default:
       return ARGP_ERR_UNKNOWN;
   }
@@ -83,13 +98,14 @@ int main(int argc, char** argv) {
   arguments.experiment_duration = DEFAULT_EXPERIMENT_DURATION;
   arguments.iteration_count = DEFAULT_ITERATION_COUNT;
   arguments.operation = DEFAULT_OPERATION;
+  arguments.sync = DEFAULT_SYNC;
   // Parse args
   argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
   struct timespec start, end;
   int i;
 
-  init(0);
+  init(arguments.sync);
   mallocCPU(arguments.data_size);
 
   for (i = 0; i < arguments.iteration_count; ++i) {
@@ -113,6 +129,14 @@ int main(int argc, char** argv) {
       break;
     }
     fprintf(stdout, "%3ld,\n", elapsed_ns(&start, &end));
+    
+    // Sleep for small amount of time to emulate periodicity.
+    {
+      struct timespec delay;
+      delay.tv_sec = 0;
+      delay.tv_nsec = FIVE_MS_IN_NS;
+      nanosleep(&delay, NULL);
+    }
   }
   freeCPU();
   finish();
