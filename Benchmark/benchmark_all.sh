c8265f6885d59308deea04e18bee0b653bc8cc40
Vance Miller <vbmiller@cs.unc.edu>
2016-05-05 14:38:18 +0000

    update benchmark script

--- Benchmark/benchmark_all.sh
+++ Benchmark/benchmark_all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-iterations=10000
+duration=30*60 #30 minutes
+sync=0 #spin
 #Samples are sd, sf, fasthog
 for sample in sd_c sf_c fasthog_c sd_zc sf_zc
 do
   all_out="SCHED_OTHER/${sample}/"
   mkdir -p $all_out 
-  stdbuf -oL ./benchmark_${sample} -n${iterations} --all > $all_out/results.csv
+  stdbuf -oL ./benchmark_${sample} -y${sync} -d${duration} --all > $all_out/results.csv
 done
 
