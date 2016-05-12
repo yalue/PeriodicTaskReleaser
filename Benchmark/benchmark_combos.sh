574a18ef312263135ec85ab28cd0a5bb47fb08d3
Vance Miller <vbmiller@cs.unc.edu>
2016-05-12 21:23:40 +0000

    Add benchmarking for combinations of programs.
    - sd, hog, dnn
     - fix 1, choose all combinations 0-3
    - sd, hog, dnn + 0-3 mm 2^20,

---
+++ Benchmark/benchmark_combos.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+
+duration=$((4*60)) #4 minutes
+size=$((2^20))
+
+# CPU programs
+for copy in c zc
+do
+  for sample in sd fasthog convbench 
+  do
+    out="1gpu/${copy}/${sample}"
+    mkdir -p $out 
+    echo $out
+    ./run_experiment.sh ./benchmark_${sample}_${copy} ${duration} ${size} ${out}/${sample}_${copy}
+  done
+done
+
+# 2 GPU programs
+for copy in c zc
+do
+  for fixed in sd fasthog convbench
+  do
+    for sample in sd fasthog convbench 
+    do
+      if [ "$fixed" = "$sample" ]
+      then
+        continue
+      fi
+      out="2gpu/${copy}/${fixed}/${sample1}"
+      mkdir -p $out 
+      echo $out
+      i=0
+      ./run_experiment.sh ./benchmark_${fixed}_${copy} ${duration} ${size} ${out}/${fixed}_${copy} &
+      pids[$i]=$!
+      i=$(($i+1))
+      ./run_experiment.sh ./benchmark_${sample}_${copy} ${duration} ${size} ${out}/${sample}_${copy} &
+      pids[$i]=$!
+      i=$(($i+1))
+      for pid in ${pids[@]}
+      do
+        wait $pid
+      done
+    done
+  done
+done
+
+# 3 GPU programs
+for copy in c zc
+do
+  for fixed in sd fasthog convbench
+  do
+    for sample1 in sd fasthog convbench 
+    do
+      if [ "$fixed" = "$sample1" ]
+      then
+        continue
+      fi
+      for sample2 in sd fasthog convbench
+      do
+        if [[ ("$fixed" = "$sample2") || ("$sample1" = "$sample2") ]]
+        then
+          continue
+        fi
+        out="3gpu/${copy}/${fixed}/${sample1}/${sample2}"
+        mkdir -p $out 
+        echo $out
+        i=0
+        ./run_experiment.sh ./benchmark_${fixed}_${copy} ${duration} ${size} ${out}/${fixed}_${copy} &
+        pids[$i]=$!
+        i=$(($i+1))
+        ./run_experiment.sh ./benchmark_${sample1}_${copy} ${duration} ${size} ${out}/${sample1}_${copy} &
+        pids[$i]=$!
+        i=$(($i+1))
+        ./run_experiment.sh ./benchmark_${sample2}_${copy} ${duration} ${size} ${out}/${sample2}_${copy} &
+        pids[$i]=$!
+        i=$(($i+1))
+        for pid in ${pids[@]}
+        do
+          wait $pid
+        done
+      done
+    done
+  done
+done
+
+# 4 GPU programs
+for copy in c zc
+do
+  for fixed in sd fasthog convbench
+  do
+    for sample1 in sd fasthog convbench 
+    do
+      if [ "$fixed" = "$sample1" ]
+      then
+        continue
+      fi
+      for sample2 in sd fasthog convbench
+      do
+        if [[ ("$fixed" = "$sample2") || ("$sample1" = "$sample2") ]]
+        then
+          continue
+        fi
+        for sample3 in sd fasthog convbench
+        do
+          if [[ ("$fixed" = "$sample3") || ("$sample1" = "$sample3") || ("$sample2" = "$sample3") ]]
+          then
+            continue
+          fi
+          out="4gpu/${copy}/${fixed}/${sample1}/${sample2}/${sample3}"
+          mkdir -p $out 
+          echo $out
+          i=0
+          ./run_experiment.sh ./benchmark_${fixed}_${copy} ${duration} ${size} ${out}/${fixed}_${copy} &
+          pids[$i]=$!
+          i=$(($i+1))
+          ./run_experiment.sh ./benchmark_${sample1}_${copy} ${duration} ${size} ${out}/${sample1}_${copy} &
+          pids[$i]=$!
+          i=$(($i+1))
+          ./run_experiment.sh ./benchmark_${sample2}_${copy} ${duration} ${size} ${out}/${sample2}_${copy} &
+          pids[$i]=$!
+          i=$(($i+1))
+          ./run_experiment.sh ./benchmark_${sample3}_${copy} ${duration} ${size} ${out}/${sample3}_${copy} &
+          pids[$i]=$!
+          i=$(($i+1))
+          for pid in ${pids[@]}
+          do
+            wait $pid
+          done
+        done
+      done
+    done
+  done
+done
+
+
+
+# Fixed GPU + 0-3 MM 2^20
+for copy in c zc
+do
+  for sample in sd fasthog convbench 
+  do
+    for j in `seq 0 3`
+    do
+      out="gpu_mm/${copy}/${sample}/${j}"
+      mkdir -p $out 
+      echo $out
+      i=0
+      ./run_experiment.sh ./benchmark_${sample}_${copy} ${duration} ${size} ${out}/${sample}_${copy} &
+      pids[$i]=$!
+      i=$(($i+1))
+      for k in `seq 0 $j`
+      do
+        ./run_experiment.sh ./benchmark_mm_${copy} ${duration} ${size} ${out}/mm_${copy} &
+        pids[$i]=$!
+        i=$(($i+1))
+      done
+      for pid in ${pids[@]}
+      do
+        wait $pid
+      done
+    done
+  done
+done
+
