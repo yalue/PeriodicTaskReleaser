63744cfdc330487266828df74ba1864a1ec44124
Vance Miller <vbmiller@cs.unc.edu>
2016-05-13 15:48:27 +0000

    fix output file names

--- Benchmark/benchmark_mem_intensive.sh
+++ Benchmark/benchmark_mem_intensive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-duration=$((4*60)) #4 minutes
+duration=$((10*60)) #10 minutes
 size=$((2^20))
 
 # Fixed GPU + 0-3 VA 2^20
@@ -8,20 +8,20 @@ for copy in c zc
 do
   for j in `seq 0 3`
   do
-    out="gpu_va/${copy}/${sample}/${j}"
+    out="gpu_va/${copy}/${j}"
     mkdir -p $out 
     echo $out
     i=0
-    ./run_experiment.sh ./benchmark_sd_${copy} ${duration} ${size} ${out}/${sample}_${copy} &
+    ./run_experiment.sh ./benchmark_sd_${copy} ${duration} ${size} ${out}/sd_${copy} &
     pids[$i]=$!
     i=$(($i+1))
-    ./run_experiment.sh ./benchmark_fasthog_${copy} ${duration} ${size} ${out}/${sample}_${copy} &
+    ./run_experiment.sh ./benchmark_fasthog_${copy} ${duration} ${size} ${out}/fasthog_${copy} &
     pids[$i]=$!
     i=$(($i+1))
-    ./run_experiment.sh ./benchmark_convbench_${copy} ${duration} ${size} ${out}/${sample}_${copy} &
+    ./run_experiment.sh ./benchmark_convbench_${copy} ${duration} ${size} ${out}/convbench_${copy} &
     pids[$i]=$!
     i=$(($i+1))
-    for k in `seq 0 $j`
+    for k in `seq 1 $j`
     do
       ./run_experiment.sh ./benchmark_va_cpu ${duration} ${size} ${out}/va_cpu_${k} &
       pids[$i]=$!
