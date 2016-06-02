651b1e469c2fdb07f472e8c9432ce62f92488658
Nathan O <otternes@smithfd-arm-dev4.cs.unc.edu>
2016-06-02 10:11:10 -0400

    Start on nvprof analysis
     - Added process_csv_profiles.rb script, which parses nvprof data.
    
     - Added 2 directories of nvprof data, for 2sd_2hog and sd_3mmul.

--- Benchmark/combinations_5-14.sh
+++ Benchmark/combinations_5-14.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Configuration
-duration=$((10*60)) # 10 minutes
+duration=$((10))
 size=$((2^22))
 randsleep="--randsleep"
 copy="c"
 
 args="$copy $duration $size $randsleep"
 
+: <<'END'
 # Experiments
 echo "1/
 1_fasthog/
@@ -33,34 +34,14 @@ echo "3/
 2_convbench_1_fasthog/
 2_convbench_1_sd/
 3_convbench/" | ./run_benchmark_parse.sh $args
+END
 
 echo "4/
-4_fasthog/
-1_sd_3_fasthog/
-2_sd_2_fasthog/
-3_sd_1_fasthog/
-4_sd/
-1_convbench_3_fasthog/
-1_convbench_1_sd_2_fasthog/
-1_convbench_2_sd_1_fasthog/
-1_convbench_3_sd/
-2_convbench_2_fasthog/
-2_convbench_1_sd_1_fasthog/
-2_convbench_2_sd/
-3_convbench_1_fasthog/
-3_convbench_1_sd/
-4_convbench/" | ./run_benchmark_parse.sh $args
+2_sd_2_fasthog/" | ./run_benchmark_parse.sh $args
 
+: <<'END'
 echo "MM/
-1_mm_1_sd/
-2_mm_1_sd/
-3_mm_1_sd/
-1_mm_1_convbench/
-2_mm_1_convbench/
-3_mm_1_convbench/
-1_mm_1_fasthog/
-2_mm_1_fasthog/
-3_mm_1_fasthog/" | ./run_benchmark_parse.sh $args
+3_mm_1_sd/" | ./run_benchmark_parse.sh $args
 
 # Vector add is on CPU by default
 echo "VA_CPU/
@@ -73,4 +54,4 @@ echo "VA_CPU/
 1_va_1_fasthog/
 2_va_1_fasthog/
 3_va_1_fasthog/" | ./run_benchmark_parse.sh $args
-
+END
