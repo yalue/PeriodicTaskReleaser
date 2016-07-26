72f62908c3f054b840ea1eca6e38d447824e694b
Nathan O <otternes@cs.unc.edu>
2016-07-26 09:11:13 -0400

    Reformat copy version of SD pre-modifications

--- Samples/Copy/StereoDisparity/sd.cu
+++ Samples/Copy/StereoDisparity/sd.cu
@@ -33,44 +33,35 @@
  *
  */
 
-// includes, system
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <sys/types.h>
-#include <sys/mman.h>
-#include <unistd.h>
-#include <sched.h>
 #include <errno.h>
-
-// includes, kernels
-// For the CUDA runtime routines (prefixed with "cuda")
+#include <math.h>
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <cuda_runtime.h>
-// the kernel code
-#include "sd_kernel.cuh"
-// GPUSync header
- extern "C" {
+#include <helper_cuda.h>
+#include <helper_functions.h>
+extern "C" {
 #include "gpusync.h"
- }
-
-// includes, project
-#include <helper_functions.h>  // helper for shared that are common to CUDA Samples
-#include <helper_cuda.h>       // helper for checking cuda initialization and error checking
-#include <helper_string.h>     // helper functions for string parsing
+}
+#include "sd_kernel.cuh"
 
 // Stream for the thread's GPU Operations
- cudaStream_t stream;
+cudaStream_t stream;
 
 // Host Memory
- unsigned int *h_odata;
- unsigned char *h_img0;
- unsigned char *h_img1;
+unsigned int *h_odata;
+unsigned char *h_img0;
+unsigned char *h_img1;
 
 // Device memory
- unsigned int *d_odata;
- unsigned int *d_img0;
- unsigned int *d_img1;
+unsigned int *d_odata;
+unsigned int *d_img0;
+unsigned int *d_img1;
 
 // Kernel execution parameters
 unsigned int w, h;
@@ -95,48 +86,35 @@ int iDivUp(int a, int b) {
   return ((a % b) != 0) ? (a / b + 1) : (a / b);
 }
 
-////////////////////////////////////////////////////////////////////////////////
-//! CUDA Sample for calculating depth maps
-////////////////////////////////////////////////////////////////////////////////
-
 // Override helper_image.h
 inline bool loadPPM4ub(const char *file, unsigned char **data,
   unsigned int *w, unsigned int *h) {
   unsigned char *idata = 0;
   unsigned int channels;
 
-  if (__loadPPM(file, &idata, w, h, &channels)) {
-    // pad 4th component
-    int size = *w **h;
-    // keep the original pointer
-    unsigned char *idata_orig = idata;
-    checkCudaErrors(cudaMallocHost(data, sizeof(unsigned char) * size * 4));
-    unsigned char *ptr = *data;
-
-    for (int i=0; i<size; i++) {
-      *ptr++ = *idata++;
-      *ptr++ = *idata++;
-      *ptr++ = *idata++;
-      *ptr++ = 0;
-    }
-
-    free(idata_orig);
-    return true;
-  } else {
+  if (!__loadPPM(file, &idata, w, h, &channels)) {
     free(idata);
     return false;
   }
+  // pad 4th component
+  int size = *w * *h;
+  // keep the original pointer
+  unsigned char *idata_orig = idata;
+  checkCudaErrors(cudaMallocHost(data, sizeof(unsigned char) * size * 4));
+  unsigned char *ptr = *data;
+  for (int i = 0; i < size; i++) {
+    *ptr++ = *idata++;
+    *ptr++ = *idata++;
+    *ptr++ = *idata++;
+    *ptr++ = 0;
+  }
+  free(idata_orig);
+  return true;
 }
 
 
 
 extern "C" void init(int sync_level) {
-  /*
-   * The sync_level parameter is an integer that indicates the desired level of
-   * synchronization used by the GPU driver (values defined below).  The
-   * specified level is used in cudaSetDeviceFlags() to set the level
-   * prior to initialization.
-   */
    switch (sync_level) {
     case 0:
       cudaSetDeviceFlags(cudaDeviceScheduleSpin);
@@ -151,21 +129,15 @@ extern "C" void init(int sync_level) {
       fprintf(stderr, "Unknown sync level: %d\n", sync_level);
       break;
   }
-
   // Follow convention and initialize CUDA/GPU
   // used here to invoke initialization of GPU locking
   cudaFree(0);
-
   // Pin code
   if(!mlockall(MCL_CURRENT | MCL_FUTURE)) {
     fprintf(stderr, "Failed to lock code pages.\n");
     exit(EXIT_FAILURE);
   }
- 
-  // Set the device context 
   cudaSetDevice(0);
- 
-  // create a user defined stream
   cudaStreamCreate(&stream);
 }
 
@@ -177,24 +149,21 @@ extern "C" void mallocCPU(int numElements) {
   // PPM images are loaded into 4 byte/pixel memory (RGBX)
   h_img0 = NULL;
   h_img1 = NULL;
-
   if (!loadPPM4ub(fname0, &h_img0, &w, &h)) {
     fprintf(stderr, "Failed to load <%s>\n", fname0);
     exit(-1);
   }
-
   if (!loadPPM4ub(fname1, &h_img1, &w, &h)) {
     fprintf(stderr, "Failed to load <%s>\n", fname1);
     exit(-1);
   }
-
   // set up parameters used in the rest of program
   numThreads = dim3(blockSize_x, blockSize_y, 1);
   numBlocks = dim3(iDivUp(w, numThreads.x), iDivUp(h, numThreads.y));
   numData = w * h;
   memSize = sizeof(int) * numData;
 
-  //allocate memory for the result on host side
+  // allocate memory for the result on host side
   checkCudaErrors(cudaMallocHost(&h_odata, memSize));
 
   // more setup for using the GPU
@@ -218,79 +187,48 @@ extern "C" void mallocGPU(int unused) {
   checkCudaErrors(cudaMalloc((void **) &d_odata, memSize));
   checkCudaErrors(cudaMalloc((void **) &d_img0, memSize));
   checkCudaErrors(cudaMalloc((void **) &d_img1, memSize));
-
-  checkCudaErrors(cudaBindTexture2D(&offset, tex2Dleft, d_img0, ca_desc0, w, h, w*4));
+  checkCudaErrors(cudaBindTexture2D(&offset, tex2Dleft, d_img0, ca_desc0, w, h,
+    w * 4));
   assert(offset == 0);
-
-  checkCudaErrors(cudaBindTexture2D(&offset, tex2Dright, d_img1, ca_desc1, w, h, w*4));
+  checkCudaErrors(cudaBindTexture2D(&offset, tex2Dright, d_img1, ca_desc1, w,
+    h, w * 4));
   assert(offset == 0);
-
 }
 
 extern "C" void copyin(int unused) {
   // copy host memory with images to device
-  // this call is asynchronous so only the lock of CE can be handled in the wrapper
   checkCudaErrors(cudaMemcpyAsync(d_img0,  h_img0, memSize, cudaMemcpyHostToDevice, stream));
-
-  // this call is asynchronous so only the lock of CE can be handled in the wrapper
   checkCudaErrors(cudaMemcpyAsync(d_img1,  h_img1, memSize, cudaMemcpyHostToDevice, stream));
-
   // copy host memory that was set to zero to initialize device output
-  // this call is asynchronous so only the lock of CE can be handled in the wrapper
   checkCudaErrors(cudaMemcpyAsync(d_odata, h_odata, memSize, cudaMemcpyHostToDevice, stream));
-
-  // synchronize with the stream
-  // the wrapper for this function releases any lock held (CE here)
   cudaStreamSynchronize(stream);
 }
 
 extern "C" void exec(int unused) {
-  // launch the stereoDisparity kernel
-  // lock of EE is handled in wrapper for cudaLaunch()
   stereoDisparityKernel<<<numBlocks, numThreads, 0, stream>>>(d_img0, d_img1, d_odata, w, h, minDisp, maxDisp);
-
-  // synchronize with the stream after kernel execution
-  // the wrapper for this function releases any lock held (EE here)
   cudaStreamSynchronize(stream);
-
-  // Check to make sure the kernel didn't fail
   getLastCudaError("Kernel execution failed");
 }
 
 extern "C" void copyout() {
-  //Copy result from device to host for verification
-  // this call is asynchronous so only the lock of CE can be handled in the wrapper
   checkCudaErrors(cudaMemcpyAsync(h_odata, d_odata, memSize, cudaMemcpyDeviceToHost, stream));
-
-  // synchronize with the stream
-  // the wrapper for this function releases any lock held (CE here)
   cudaStreamSynchronize(stream);
 }
 
-extern "C" void freeGPU() { 
-  // cleanup device memory
+extern "C" void freeGPU() {
   checkCudaErrors(cudaFree(d_odata));
   checkCudaErrors(cudaFree(d_img0));
   checkCudaErrors(cudaFree(d_img1));
 }
 
 extern "C" void freeCPU() {
-  // cleanup host memory
   cudaFreeHost(h_odata);
   cudaFreeHost(h_img0);
   cudaFreeHost(h_img1);
 }
 
 extern "C" void finish() {
-  // clean up the user allocated stream
   cudaStreamSynchronize(stream);
-   // finish clean up with deleting the user-created stream
   cudaStreamDestroy(stream);
-
-  // cudaDeviceReset causes the driver to clean up all state. While
-  // not mandatory in normal operation, it is good practice.  It is also
-  // needed to ensure correct operation when the application is being
-  // profiled. Calling cudaDeviceReset causes all profile data to be
-  // flushed before the application exits
   checkCudaErrors(cudaDeviceReset());
 }
